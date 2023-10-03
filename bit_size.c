02b24929cbd74ef107aef879131944be183b6cc0
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2023-10-03 20:20:02 +0530

    Code for day 16 (03/10/2023)

---
+++ bit_size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char c;int a;float b; double d;long int li;
+    printf("%d\n",sizeof(c));
+    printf("%d\n",sizeof(a));
+    printf("%d\n",sizeof(b));
+    printf("%d\n",sizeof(d));
+    printf("%d\n",sizeof(li));
+    getch();
+    return 0;
+
+}
